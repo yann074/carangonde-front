6bca57d21c86a7bd3922d9e519bbc7deedc71813
yann074 <yannsmith2907@gmail.com>
2025-05-11 13:55:31 -0300

    feat: update to layout courses

--- src/pages/courses/myCourses.tsx
+++ src/pages/courses/myCourses.tsx
@@ -94,7 +94,7 @@ export const MyCourses: React.FC = () => {
       .then((response) => {
         const updatedData = response.data.data.map((course: any) => ({
           ...course,
-          image_url: `https://carangonde-back-production.up.railway.app/storage/${course.image}`,
+          image_url: {course.image}`,
           pdf_url: `https://carangonde-back-production.up.railway.app/storage/${course.pdf}`,
         }))
 
